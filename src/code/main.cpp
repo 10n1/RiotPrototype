824eb5072593c0f7e0bc0a4d81159b03e20a22df
Omar A Rodriguez <omar@gnlnx.net>
2010-12-05 20:42:18 -0800

    initial math stuff

--- src/code/main.cpp
+++ src/code/main.cpp
@@ -8,6 +8,11 @@ int main()
     printf( "Hello world! %d\n", nTemp );
 
     int* pTest = new int[100];
+    float2* pVec2 = new float2();
+    float3* pVec3 = new float3();
+    float4* pVec4 = new float4();
+    float3x3* pVec3x3 = new float3x3();
+    float4x4* pVec4x4 = new float4x4();
 
     // DON'T CODE BELOW THIS LINE
     DumpMemoryLeaks();
