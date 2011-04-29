0dc9b767d1101f3bff2159d3554c07ede81fca77
Kyle Weicht <kweicht@gmail.com>
2011-04-29 14:44:39 -0700

    10k objects at 730 FPS :) Processing messages serially works for now...10k objects at 730 FPS :)

--- src/scene/ObjectManager.cpp
+++ src/scene/ObjectManager.cpp
@@ -2,7 +2,7 @@
 File:           ObjectManager.cpp
 Author:         Kyle Weicht
 Created:        4/17/2011
-Modified:       4/29/2011 2:39:32 PM
+Modified:       4/29/2011 2:43:50 PM
 Modified by:    Kyle Weicht
 \*********************************************************/
 #include "ObjectManager.h"
@@ -240,9 +240,9 @@ namespace Riot
         pTaskManager->WaitForCompletion( nProcessTask );
 
         // ...then resolve any discrepencies and handle messages
-        task_handle_t nMessageTask = pTaskManager->PushTask( ParallelProcessComponentMessages, this, m_nNumMessages, 16 );
-        pTaskManager->WaitForCompletion( nMessageTask );
-        //ParallelProcessComponentMessages( this, 0, 0, m_nNumMessages );
+        //task_handle_t nMessageTask = pTaskManager->PushTask( ParallelProcessComponentMessages, this, m_nNumMessages, 16 );
+        //pTaskManager->WaitForCompletion( nMessageTask );
+        ParallelProcessComponentMessages( this, 0, 0, m_nNumMessages );
 #else
         ParallelProcessComponents( this, 0, 0, eNUMCOMPONENTS );
         ParallelProcessComponentMessages( this, 0, 0, m_nNumMessages );
